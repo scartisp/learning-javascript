7c6bfac6e01466ee02e885cb858028f0349a3293
simion cartis <simion.cartis1@gmail.comgit config --global user.name simion>
2025-08-29 23:55:52 -0400

    added branch with completed projects

--- scripts/button-things.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('button-container').addEventListener('click', function (event) { //event is of type object, includes what triggered the event listener,
-  //where the clicke happened, modifier keys, etc. this event handler will always be passed as the first parameter for an event listener function
-  if (event.target.classList.contains('js-button')) { //event.target is the thing that triggers the event listener. we check to see if it is a button that contains the needed class
-    this.querySelectorAll('.js-button').forEach(btn => {
-      btn.classList.remove('selected')
-    })
-    event.target.classList.add('selected')
-  }
-})
