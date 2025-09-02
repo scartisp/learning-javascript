7f872ca2b2ff2ba5e8d0adcf7bb75fde402bd101
simion cartis <simion.cartis1@gmail.comgit config --global user.name simion>
2025-09-01 22:39:03 -0400

    started on final todo project version

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -6,35 +6,67 @@ function saveList() {
   localStorage.setItem('list', JSON.stringify(todoList));
 }
 
-function addToList1() {
+
+function addToList() {
   const inputElement = document.querySelector('.js-todo-input'); // should reference the actual html element, not just the value,
   // because that would just be a local copy of the string it holds
   todoList.push(inputElement.value);
-  console.log(todoList);
-  inputElement.value = '';
-  //saveList();
-}
-
-function addToList2() {
-  const inputElement = document.querySelector('.js-todo-input2'); // should reference the actual html element, not just the value,
-                                                                  // because that would just be a local copy of the string it holds
-  todoList.push(inputElement.value);
+  
+  
   console.log(todoList)
   displayList(todoList);
   inputElement.value = '';
   //saveList();
 }
 
+function deleteFromList() {
+  
+}
+
 function displayList(todoList) {
   const ouptutElement = document.querySelector('.js-todo-output');
   ouptutElement.innerHTML = '';
   for (i = 0; i < todoList.length; ++i) {
-    console.log(i);
-    i != todoList.length - 1 ? ouptutElement.innerHTML += todoList[i] + '<br>' : ouptutElement.innerHTML += todoList[i];
+    ouptutElement.innerHTML += `<p>
+    ${todoList[i]}
+    <button onclick="
+    todoList.splice(${i},1);
+    displayList(todoList);
+    ">Delete</button> 
+    </p>`;
   }
 }
 
 
+// TODO LIST PRACTICE 1
+// function addToList1() {
+//   const inputElement = document.querySelector('.js-todo-input'); // should reference the actual html element, not just the value,
+//   // because that would just be a local copy of the string it holds
+//   todoList.push(inputElement.value);
+//   console.log(todoList);
+//   inputElement.value = '';
+//   //saveList();
+// }
+
+//TODO LIST PRACTICE 2
+// function addToList2() {
+//   const inputElement = document.querySelector('.js-todo-input2'); // should reference the actual html element, not just the value,
+//                                                                   // because that would just be a local copy of the string it holds
+//   todoList.push(inputElement.value);
+//   console.log(todoList)
+//   displayList(todoList);
+//   inputElement.value = '';
+//   //saveList();
+// }
+
+// function displayList(todoList) {
+//   const ouptutElement = document.querySelector('.js-todo-output');
+//   ouptutElement.innerHTML = '';
+//   for (i = 0; i < todoList.length; ++i) {
+//     console.log(i);
+//     i != todoList.length - 1 ? ouptutElement.innerHTML += todoList[i] + '<br>' : ouptutElement.innerHTML += todoList[i];
+//   }
+// }
 
 
 
@@ -55,9 +87,9 @@ const countWords = ['apple', 'apple', 'grape', 'apple'];
 const count = {};
 for (let word of countWords) {
   if (count[word]) //what this is doing is creating a number data memeber to count
-                   // with the string as the variable name
-    ++count[word];//
-   else
+  // with the string as the variable name
+  ++count[word];//
+  else
     count[word] = 1;
 }
 console.log(count);
