d350c9fa8aac78f4b6911fcc9afe597d0ed81ba1
scartisp <simion.cartis1@gmail.com>
2025-11-19 22:50:48 -0500

    added eventListners for clicking the add and delete button in the todo list

--- todo list/todo-list.js
+++ todo list/todo-list.js
@@ -1,18 +1,28 @@
+//DOM stuff
+const taskInputElement = document.querySelector('.js-todo-input'); // should reference the actual html element, not just the value,
+const dateInputElement = document.querySelector('.js-date-input');
+const ouptutElement = document.querySelector('.js-todo-output');
 
-
+//loading the list from localStorage
 const todoList = JSON.parse(localStorage.getItem('list')) || []; // better to use const for objects, including arrays
 displayList(todoList);
 
-
+//saves the list
 function saveList() {
   localStorage.setItem('list', JSON.stringify(todoList));
 }
 
+//event listener for clicking add button
+document.querySelector('.js-add-button').addEventListener('click', () => {
+  addToList();
+})
 
+
+
+
+//add things to the list
 function addToList() {
-  const taskInputElement = document.querySelector('.js-todo-input'); // should reference the actual html element, not just the value,
   // because that would just be a local copy of the string it holds
-  const dateInputElement = document.querySelector('.js-date-input');
   if (taskInputElement.value !== '') { //checking to see if the value isn't empty because if not, can add empty todo in array and just display a delete button without any text
     todoList.push({
       name: taskInputElement.value,
@@ -23,22 +33,28 @@ function addToList() {
   taskInputElement.value = dateInputElement.value = '';
 }
 
+
+//displays the list
 function displayList(todoList) {
-  const ouptutElement = document.querySelector('.js-todo-output');
   ouptutElement.innerHTML = '';
 
-  console.log(todoList.length);
   for (i = 0; i < todoList.length; ++i) {
     ouptutElement.innerHTML += `
     <div>${todoList[i].name}</div>
-     <div>${todoList[i].dueDate} </div>
-    <button class="todo-delete-button" onclick="
-    todoList.splice(${i},1);
-    displayList(todoList);
-    ">Delete</button> 
+    <div>${todoList[i].dueDate} </div>
+    <button class="todo-delete-button js-todo-delete-button"">Delete</button> 
     `; // everythiing is in its own individual div because of how this section is styled (see todo-list.css).
     // The button is not in a div because it already has an html element.
     // Div is chosen because it has no default styling
   }
-  saveList();
+
+  document.querySelectorAll('.js-todo-delete-button').forEach((deleteButton, i) => {
+    deleteButton.addEventListener('click', () => {
+      todoList.splice(i, 1);
+      displayList(todoList);
+    })
+  }); // must be added after adding the todo and within the displayList function. this is because display list deletes everything in outputElement.innerHTML,
+  // so everytime it is called, and the list is re-created, you need to re-add event listeners
 }
+
+saveList();
